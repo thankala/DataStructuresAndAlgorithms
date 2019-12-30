ef01228ae32156d6fc97130ea0c1827a8a922f5f
thankala <kalatheristhanasis@gmail.com>
2019-12-30 19:59:59 +0200

    Finished Bubble Sort

--- SortAlgos/BubbleSort/src/com/thankala/Main.java
+++ SortAlgos/BubbleSort/src/com/thankala/Main.java
@@ -6,8 +6,27 @@ public class Main {
 
         int[] myArray = new int[5];
         myArray[0] = 5;
-        myArray[0] = 5;
+        myArray[1] = 11;
+        myArray[2] = 2221;
+        myArray[3] = 527;
+        myArray[4] = 0;
 
-        myArray[0] = 5;
+        int a = 0;
+
+        for(int k=myArray.length-1;k>0;k--){
+            for(int i=0;i<k;i++){
+                if(myArray[i]>myArray[i+1]){
+                    a= myArray[i+1];
+                    myArray[i+1] = myArray[i];
+                    myArray[i]=a;
+                }else{
+                    continue;
+                }
+            }
+        }
+
+        for(int i=0;i<myArray.length;i++){
+            System.out.println(myArray[i]);
+        }
     }
 }
